733a54279eb4d60bb3b2b68f0766d13cb89c507a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:54:05 +0700

    drop `esnext.string.to-well-formed` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2249,8 +2249,6 @@ export const data = {
   'esnext.string.match-all': null,
   // TODO: Remove from `core-js@4`
   'esnext.string.replace-all': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.string.to-well-formed': null,
   'esnext.symbol.async-dispose': {
     deno: '1.38',
     // Node 20.4.0 add `Symbol.asyncDispose`, but with incorrect descriptor
@@ -2552,7 +2550,6 @@ export const renamed = new Map([
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.string.match-all', 'es.string.match-all'],
   ['esnext.string.replace-all', 'es.string.replace-all'],
-  ['esnext.string.to-well-formed', 'es.string.to-well-formed'],
   ['web.url', 'web.url.constructor'],
   ['web.url-search-params', 'web.url-search-params.constructor'],
 ]);
