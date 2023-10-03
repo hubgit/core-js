c193166a612d77aaa289a11cbedffde9d260187d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:16:34 +0700

    drop `esnext.symbol.pattern-match` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2203,9 +2203,6 @@ export const data = {
   'esnext.symbol.observable': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.symbol.pattern-match': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.symbol.replace-all': {
   },
   'esnext.typed-array.filter-reject': {
