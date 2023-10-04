d6333f404305e35b80be6a466cf140c3ac9c6c19
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:39:37 +0700

    rename `web.url-search-params` -> `web.url-search-params.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2350,8 +2350,6 @@ export const data = {
     node: '10.0',
     safari: '14.0',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'web.url-search-params': null,
   'web.url-search-params.constructor': {
     bun: '0.1.1',
     chrome: '67',
@@ -2389,7 +2387,6 @@ export const renamed = new Map([
   ['esnext.map.group-by', 'es.map.group-by'],
   ['esnext.object.group-by', 'es.object.group-by'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
-  ['web.url-search-params', 'web.url-search-params.constructor'],
 ]);
 
 for (const [old, nw] of renamed) data[old] = data[nw];
@@ -2417,7 +2414,6 @@ export const ignored = [
   'web.set-immediate',
   'web.set-interval',
   'web.set-timeout',
-  'web.url-search-params.constructor',
 ];
 
 for (const ignore of ignored) delete data[ignore];
